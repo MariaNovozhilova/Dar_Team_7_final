200baa470c906ff23e83cea9ffc86b3758e026ce
Maria Novozhilova <95442759+MariaNovozhilova@users.noreply.github.com>
2024-08-01 23:28:53 +0300

    Update bad_data_collection.py
    made it work finally

--- Maria_dar_etl_team7/dags/scripts/bad_data_collection.py
+++ Maria_dar_etl_team7/dags/scripts/bad_data_collection.py
@@ -1,8 +1,15 @@
 import psycopg
 import json
+import os
 
-f = open('etl_base.json')
-dsn_source = json.load(f)
+
+def create_cfg_variable():
+    f = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'etl_base.json'))
+    dsn_ods = json.load(f)
+    conn_d = create_connection(dsn_ods["db_name"], dsn_ods["db_user"], dsn_ods["db_password"],
+                                       dsn_ods["db_host"], dsn_ods["db_port"])
+    return conn_d
+
 
 def create_connection(db_name, db_user, db_password, db_host, db_port):
     connection = None
@@ -19,8 +26,6 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
         print(f"The error '{e}' occurred")
     return connection
 
-conn = create_connection(dsn_source["db_name"], dsn_source["db_user"], dsn_source["db_password"], dsn_source["db_host"], dsn_source["db_port"])
-
 tables = ['dbms', 'dbms_and_employee_grade', 'domain', 'employee_education_level', 'employee', 'employee_certificate', 
           'employee_domain_experience', 'industry', 'industry_employee_experience', 'platform', 
           'platform_and_employee_grade', 'program', 'program_and_employee_grade', 'resume', 
@@ -30,7 +35,8 @@ tables = ['dbms', 'dbms_and_employee_grade', 'domain', 'employee_education_level
 #в каждую таблицу добавить столбец "причина добавления" и заполнять его для понимания, что плохо в данных
 
 bad_dict = {'bad_data.employee_certificate':'''CREATE TABLE IF NOT EXISTS bad_data.employee_certificate
-(LIKE lgc.employee_certificate); ALTER TABLE bad_data.employee_certificate ADD reason text; TRUNCATE TABLE bad_data.employee_certificate;
+(LIKE lgc.employee_certificate); ALTER TABLE bad_data.employee_certificate ADD IF NOT EXISTS reason text; 
+TRUNCATE TABLE bad_data.employee_certificate;
 INSERT INTO bad_data.employee_certificate (user_id, id, year, updated_at, title, organisation, sort, active, reason)
 SELECT user_id
 , id
@@ -45,17 +51,21 @@ FROM lgc.employee_certificate
 WHERE title LIKE '%Наименование%';'''}
 
 bad_dict['bad_data.dbms'] = '''CREATE TABLE IF NOT EXISTS bad_data.dbms
-(LIKE lgc.dbms); TRUNCATE TABLE bad_data.dbms;
-INSERT INTO bad_data.dbms (id, updated_at, sort, active, dbms)
+(LIKE lgc.dbms); ALTER TABLE bad_data.dbms ADD IF NOT EXISTS reason text; TRUNCATE TABLE bad_data.dbms;
+INSERT INTO bad_data.dbms (id, updated_at, sort, active, dbms, reason)
 SELECT id, updated_at, sort, active, dbms
+, 'big date' AS reason
 FROM lgc.dbms
 WHERE updated_at::date > NOW();'''
 
 bad_dict['bad_data.dbms_and_employee_grade'] ='''CREATE TABLE IF NOT EXISTS bad_data.dbms_and_employee_grade
-(LIKE lgc.dbms_and_employee_grade); TRUNCATE TABLE bad_data.dbms_and_employee_grade;
+(LIKE lgc.dbms_and_employee_grade); ALTER TABLE bad_data.dbms_and_employee_grade ADD IF NOT EXISTS reason text; 
+TRUNCATE TABLE bad_data.dbms_and_employee_grade;
 INSERT INTO bad_data.dbms_and_employee_grade
-(id, user_id, updated_at, sort, grade, active, "date", dbms)
-SELECT * FROM lgc.dbms_and_employee_grade
+(id, user_id, updated_at, sort, grade, active, "date", dbms, reason)
+SELECT id, user_id, updated_at, sort, grade, active, "date", dbms
+, 'duplicate' AS reason
+FROM lgc.dbms_and_employee_grade
 WHERE id IN
 	(SELECT id FROM lgc.dbms_and_employee_grade
 	EXCEPT SELECT MIN(id) FROM lgc.dbms_and_employee_grade
@@ -63,7 +73,7 @@ WHERE id IN
 ORDER BY user_id, sort, grade, dbms; '''
 
 bad_dict['bad_data.employee_education_level'] = '''CREATE TABLE IF NOT EXISTS bad_data.employee_education_level
-(LIKE lgc.employee_education_level);  ALTER TABLE bad_data.employee_education_level ADD reason text;
+(LIKE lgc.employee_education_level);  ALTER TABLE bad_data.employee_education_level ADD IF NOT EXISTS reason text;
 TRUNCATE TABLE bad_data.employee_education_level;
 INSERT INTO bad_data.employee_education_level (user_id, id, year_graduated, updated_at, institution_name, sort, level
 , faculty_department, short_name, active, qualification, specialty, reason)
@@ -83,10 +93,13 @@ ORDER BY user_id, sort, level, year_graduated, faculty_department, specialty;
 '''
 
 bad_dict['bad_data.industry_employee_experience'] = '''CREATE TABLE IF NOT EXISTS bad_data.industry_employee_experience (LIKE lgc.industry_employee_experience);
-ALTER TABLE bad_data.industry_employee_experience ADD reason text;
+ALTER TABLE bad_data.industry_employee_experience ADD IF NOT EXISTS reason text;
 TRUNCATE TABLE bad_data.industry_employee_experience;
-INSERT INTO bad_data.industry_employee_experience
-SELECT *, 'duplicate' AS reason FROM lgc.industry_employee_experience
+INSERT INTO bad_data.industry_employee_experience (user_id, id, updated_at, sort, experience, active, date
+, industry, reason)
+SELECT user_id, id, updated_at, sort, experience, active, date, industry
+, 'duplicate' AS reason 
+FROM lgc.industry_employee_experience
 WHERE id IN
 	(SELECT id FROM lgc.industry_employee_experience
 	EXCEPT SELECT MIN(id) FROM lgc.industry_employee_experience
@@ -96,7 +109,7 @@ ORDER BY user_id, sort, industry, experience;
 
 bad_dict['bad_data.employee_domain_experience'] = '''
 CREATE TABLE IF NOT EXISTS bad_data.employee_domain_experience (LIKE lgc.employee_domain_experience);
-ALTER TABLE bad_data.employee_domain_experience ADD reason text;
+ALTER TABLE bad_data.employee_domain_experience ADD IF NOT EXISTS reason text;
 TRUNCATE TABLE bad_data.employee_domain_experience;
 INSERT INTO bad_data.employee_domain_experience
 SELECT *, 'duplicate' AS reason FROM lgc.employee_domain_experience
@@ -109,26 +122,25 @@ ORDER BY user_id, sort, domain, experience;
 
 bad_dict['bad_data.platform_and_employee_grade'] = '''
 CREATE TABLE IF NOT EXISTS bad_data.platform_and_employee_grade (LIKE lgc.platform_and_employee_grade);
-ALTER TABLE bad_data.employee_domain_experience ADD reason text;
+ALTER TABLE bad_data.platform_and_employee_grade ADD IF NOT EXISTS reason text;
 TRUNCATE TABLE bad_data.platform_and_employee_grade;
-INSERT INTO bad_data.platform_and_employee_grade(user_id, id, updated_at, sort, grade, active, "date", platform, reason)
-SELECT *, 'duplicate' AS reason FROM lgc.platform_and_employee_grade
+INSERT INTO bad_data.platform_and_employee_grade(user_id, id, updated_at, sort, grade, active, "date"
+, platform, reason)
+SELECT user_id, id, updated_at, sort, grade, active, "date"
+, platform, 'duplicate' AS reason FROM lgc.platform_and_employee_grade
 WHERE id IN
 	(SELECT id FROM lgc.platform_and_employee_grade
 	EXCEPT SELECT MIN(id) FROM lgc.platform_and_employee_grade
 	GROUP BY user_id, sort, grade, platform)
-ORDER BY user_id, sort, grade, platform;
-
-SELECT *, 'wrong platform id' 
-FROM dds.platform_and_employee_grade
-WHERE platform NOT IN (SELECT id FROM dds.platform)
+ORDER BY user_id, sort, grade, platform; 
 '''
 
 bad_dict['bad_data.program_and_employee_grade'] = '''
 CREATE TABLE IF NOT EXISTS bad_data.program_and_employee_grade (LIKE lgc.program_and_employee_grade);
+ALTER TABLE bad_data.program_and_employee_grade ADD IF NOT EXISTS reason text;
 TRUNCATE TABLE bad_data.program_and_employee_grade;
-INSERT INTO bad_data.program_and_employee_grade(user_id, id, updated_at, sort, grade, active, "date", program)
-SELECT user_id, id, updated_at, sort, grade, active, "date", program FROM lgc.program_and_employee_grade
+INSERT INTO bad_data.program_and_employee_grade(user_id, id, updated_at, sort, grade, active, "date", program, reason)
+SELECT user_id, id, updated_at, sort, grade, active, "date", program, 'duplicate' AS reason FROM lgc.program_and_employee_grade
 WHERE id::integer IN
 	(SELECT id::integer FROM lgc.program_and_employee_grade
 	EXCEPT SELECT MIN(id::integer) FROM lgc.program_and_employee_grade
@@ -139,9 +151,11 @@ ORDER BY user_id, sort, grade, program;
 
 bad_dict['bad_data.sde_and_employee_grade'] = '''CREATE TABLE IF NOT EXISTS bad_data.sde_and_employee_grade (LIKE lgc.sde_and_employee_grade);
 TRUNCATE TABLE bad_data.sde_and_employee_grade;
+ALTER TABLE bad_data.sde_and_employee_grade ADD IF NOT EXISTS reason text; 
 INSERT INTO bad_data.sde_and_employee_grade(id, updated_at, sort, sde, grade, active, 
-"date", user_id)
-SELECT * FROM lgc.sde_and_employee_grade
+"date", user_id, reason)
+SELECT id, updated_at, sort, sde, grade, active, "date", user_id, 'duplicate' AS reason
+FROM lgc.sde_and_employee_grade
 WHERE id IN
 	(SELECT id FROM lgc.sde_and_employee_grade
 	EXCEPT SELECT MIN(id) FROM lgc.sde_and_employee_grade
@@ -149,11 +163,16 @@ WHERE id IN
 ORDER BY user_id, sort, grade, sde;'''
 
 
+def bad_data():
+    conn_d = create_cfg_variable()
+    # в цикле запускаю выполнение всех запросов и заполняю bad_data слой
+    for sql in bad_dict:
+        sql = bad_dict[sql]
+        cur = conn_d.cursor()
+        cur.execute(sql)
+        conn_d.commit()
+    return 'Done'
 
 
-# в цикле запускаю выполнение всех запросов и заполняю bad_data слой
-for sql in bad_dict:
-    sql = bad_dict[sql]
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
+if __name__ == "__main__":
+    bad_data()
